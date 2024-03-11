1f7f74371c34d4f4be2b460e24126479d0f4cd52
CubanMathExpert <rafaelglez98@hotmail.com>
2024-03-11 12:07:07 -0400

    update

--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,9 +1,6 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 
-
-uniform mat4 model;
-uniform mat4 view;
 uniform mat4 projection;
 uniform vec3 offset;
 
